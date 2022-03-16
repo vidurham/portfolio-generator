c689463d7bfe3fe2bd239b1509e8a129c77a90e9
Vinny D <vdurham@prideseals.com>
2022-03-16 13:28:27 -0400

    made HTML file

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    };
+const [name, github] = profileDataArgs;
 
-    console.log('==========');
+console.log(name, github);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const pageHTML = generatePage(name, github);
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
